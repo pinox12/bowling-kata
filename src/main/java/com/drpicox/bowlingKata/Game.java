f2efd589ac3096aa1941b839691b403730e3d09f
Xavip <xpi@edu.tecnocampus.cat>
2018-05-04 09:05:52 +0200

    slide 36

--- src/main/java/com/drpicox/bowlingKata/Game.java
+++ src/main/java/com/drpicox/bowlingKata/Game.java
@@ -2,13 +2,11 @@ package com.drpicox.bowlingKata;
 
 public class Game {
 
-    private int score = 0;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 
 
     public void roll(int pins) {
-        score += pins;
         rolls[currentRoll++] = pins;
     }
 
